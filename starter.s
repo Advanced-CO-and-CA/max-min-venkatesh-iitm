a8f7af4f1974df975ecd4587fd8bc98374fbe6b5
Nitesh114 <nitesh.dps@gmail.com>
2019-09-11 20:59:01 +0530

    added starter for assignment 2

---
+++ starter.s
@@ -0,0 +1,31 @@
+
+
+
+/******************************************************************************
+* file: starter.s
+* author: G S Nitesh Narayana
+* Guide: Prof. Madhumutyam IITM, PACE
+******************************************************************************/
+
+/*
+  This is the starter code for assignment 2
+  */
+
+  @ BSS section
+      .bss
+
+  @ DATA SECTION
+      .data
+data_items: .word 10, 4, 28, 100, 8, 0 ;0 is used to terminate the data
+
+  @ TEXT section
+      .text
+
+.globl _main
+
+
+_main:
+
+      
+
+        @ you can write your own code here
